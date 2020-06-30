d1a533af8d929553e3015eb1b3ceb7ff92466d6d
paulohz <pauloh.alvesdacosta@gmail.com>
2020-06-30 01:03:25 -0300

    adding reactive forms and validations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http'
+import { ReactiveFormsModule } from '@angular/forms'
 
 import { MatToolbarModule } from '@angular/material/toolbar'
 import { MatButtonModule } from '@angular/material/button'
@@ -20,11 +22,13 @@ import { ContatoService } from './contato.service'
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    HttpClientModule,
+    ReactiveFormsModule,
 
     MatButtonModule,
     MatToolbarModule,
     MatInputModule,
-    
+
   ],
   providers: [
     ContatoService
--- src/app/contato/contato.component.ts
+++ src/app/contato/contato.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Contato } from './contato';
+import { ContatoService } from '../contato.service';
+
+import { FormBuilder, FormGroup, Validators } from '@angular/forms'
 
 @Component({
   selector: 'app-contato',
@@ -7,9 +11,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContatoComponent implements OnInit {
 
-  constructor() { }
+  formulario: FormGroup 
+
+  constructor(
+    private service: ContatoService,
+    private fb: FormBuilder
+  ) { }
 
   ngOnInit(): void {
+    
+    this.formulario = this.fb.group({
+      nome: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]]
+    })
+
+    
+  }
+  
+  submit(){
+    const erroNomeRequired = this.formulario.controls.nome.errors.required;
+    const erroEmailInvalid = this.formulario.controls.email.errors.email;
+    
+
+    console.log(this.formulario.value)
+    /*this.service.save(c).subscribe( data => {
+      console.log(data)
+    });*/
   }
 
 }
